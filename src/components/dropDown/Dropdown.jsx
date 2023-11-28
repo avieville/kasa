2dbe424ac1a508e18ec0799d0e37b608808a0b81
Aurélien Vieville <oc.aurelien.vieville@gmail.com>
2023-11-28 13:52:47 +0100

    replace term accordion by dropdown

--- src/components/dropDown/Dropdown.jsx
+++ src/components/dropDown/Dropdown.jsx
@@ -8,18 +8,18 @@ export default function Dropdown(props) {
 
   return (
     <div
-      className={`accordion ${opened ? "accordion--opened" : ""}`}
+      className={`dropdown ${opened ? "dropdown--opened" : ""}`}
       onClick={() => {
         setOpened(!opened);
       }}
     >
-      <div className="accordion__line">
-        <h3 className="accordion__title">{title}</h3>
-        <img src={arrow} className="accordion__icon" alt="arrow" />
+      <div className="dropdown__line">
+        <h3 className="dropdown__title">{title}</h3>
+        <img src={arrow} className="dropdown__icon" alt="arrow" />
       </div>
-      <div className="accordion__inner">
-        <div className="accordion__content">
-          <div className="accordion__paragraph">{children}</div>
+      <div className="dropdown__inner">
+        <div className="dropdown__content">
+          <div className="dropdown__paragraph">{children}</div>
         </div>
       </div>
     </div>
